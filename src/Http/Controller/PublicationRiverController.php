5add0e64e22f8aa2e036e90323fe7458ab692608
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-01-18 19:56:11 +0000

    feature: Add integration object on the controllers

--- src/Http/Controller/PublicationRiverController.php
+++ src/Http/Controller/PublicationRiverController.php
@@ -3,14 +3,27 @@ declare(strict_types=1);
 
 namespace JournalMedia\Sample\Http\Controller;
 
+use JournalMedia\Sample\Integration\TheJournalIE\Client as TheJournalIeIntegrationClient;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 
+/**
+ * Class PublicationRiverController
+ * @package JournalMedia\Sample\Http\Controller
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
 class PublicationRiverController
 {
     public function __invoke(ServerRequestInterface $request): ResponseInterface
     {
+        $publicationName = $request->getAttribute('publication_name', 'thejournal');
+        $theJournalIeClient = new TheJournalIeIntegrationClient;
+        $articles = $theJournalIeClient->listArticles($publicationName);
+
+        dd($articles);
+
         return new HtmlResponse(
             sprintf("Demo Mode: %s", getenv('DEMO_MODE') === "true" ? "ON" : "OFF")
         );
--- src/Http/Controller/TagRiverController.php
+++ src/Http/Controller/TagRiverController.php
@@ -3,10 +3,18 @@ declare(strict_types=1);
 
 namespace JournalMedia\Sample\Http\Controller;
 
+use JournalMedia\Sample\Integration\TheJournalIE\Client as TheJournalIeIntegrationClient;
+use JournalMedia\Sample\Service\Test;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 
+/**
+ * Class TagRiverController
+ * @package JournalMedia\Sample\Http\Controller
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
 class TagRiverController
 {
     public function __invoke(
@@ -14,6 +22,11 @@ class TagRiverController
         ResponseInterface $response,
         array $args
     ): ResponseInterface {
+        $theJournalIeClient = new TheJournalIeIntegrationClient;
+        $articles = $theJournalIeClient->listArticlesByTagName($args['tag']);
+
+        dd($articles);
+
         return new HtmlResponse(
             sprintf("Display the contents of the river for the tag '%s'", $args['tag'])
         );
