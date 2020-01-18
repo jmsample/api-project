3073b95dd7e8c15f470caf7a9c396001f1c3efcc
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-01-18 19:55:34 +0000

    feature: Create base directory for unit tests

---
+++ tests/Integration/TheJournalIe/Parser/ArticleParserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Tests\Integration\TheJournalIe\Parser;
+
+
+use Tests\TestCase;
+
+/**
+ * Class ArticleParserTest
+ * @package Tests\Integration\TheJournalIe\Parser
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+class ArticleParserTest extends TestCase
+{
+    public function testDumb()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+/**
+ * Class TestCase
+ * @package Tests
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+}
