ac165d03f77747867e87a91a13047848e6ac8de6
Jose Ruiz <Jose Ruiz>
2022-03-25 18:20:07 -0300

    pulling data on test mode to the controllers

--- src/Http/Controller/PublicationRiverController.php
+++ src/Http/Controller/PublicationRiverController.php
@@ -3,14 +3,27 @@ declare(strict_types=1);
 
 namespace JournalMedia\Sample\ApiProject\Http\Controller;
 
+use JournalMedia\Sample\ApiProject\Application\Application;
+use JournalMedia\Sample\ApiProject\Service\RiverDataSource;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Laminas\Diactoros\Response\HtmlResponse;
 
 final class PublicationRiverController
 {
+    private RiverDataSource $riverDataSource;
+    private string $publication;
+
+    public function __construct(RiverDataSource $riverDataSource, string $publication = 'thejournal')
+    {
+        $this->riverDataSource = $riverDataSource;
+        $this->publication = $publication;
+    }
+
     public function __invoke(ServerRequestInterface $request): ResponseInterface
     {
+        $data = $this->riverDataSource->get()->getArticlesByPublication($this->publication);
+        print_r($data);
         return new HtmlResponse(
             sprintf("Demo Mode: %s", $_ENV['DEMO_MODE'] === "true" ? "ON" : "OFF")
         );
--- src/Http/Controller/TagRiverController.php
+++ src/Http/Controller/TagRiverController.php
@@ -3,16 +3,25 @@ declare(strict_types=1);
 
 namespace JournalMedia\Sample\ApiProject\Http\Controller;
 
+use JournalMedia\Sample\ApiProject\Service\RiverDataSource;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Laminas\Diactoros\Response\HtmlResponse;
 
 final class TagRiverController
 {
+    private RiverDataSource $riverDataSource;
+
+    public function __construct(RiverDataSource $riverDataSource)
+    {
+        $this->riverDataSource = $riverDataSource;
+    }
     public function __invoke(
         ServerRequestInterface $request,
         array $args
     ): ResponseInterface {
+        $data = $this->riverDataSource->get()->getArticlesByPublication($request->getAttribute('tag'));
+        print_r($data);
         return new HtmlResponse(
             "Display the contents of the river for the tag '{$args['tag']}'"
         );
