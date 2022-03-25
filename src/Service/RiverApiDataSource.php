0eead9209a4abbc838a0795e8749c37e4e9ed077
Jose Ruiz <Jose Ruiz>
2022-03-25 16:18:21 -0300

    Data source classes

---
+++ src/Service/RiverApiDataSource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace JournalMedia\Sample\ApiProject\Service;
+
+use GuzzleHttp\Exception\GuzzleException;
+use JournalMedia\Sample\ApiProject\Connector\JournalApiConnector;
+
+class RiverApiDataSource implements RiverDatasourceInterface
+{
+
+    private JournalApiConnector $apiConnector;
+
+    public function __construct(JournalApiConnector $apiConnector)
+    {
+        $this->apiConnector = $apiConnector;
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getArticlesByPublication(string $publicationName, array $extraOptions = [])
+    {
+        $response = $this->apiConnector->doGet(DIRECTORY_SEPARATOR . $publicationName);
+        return $response['result'] ?? [];
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getArticlesByTag(string $tagName, array $extraOptions = [])
+    {
+        $response = $this->apiConnector->doGet(DIRECTORY_SEPARATOR . $tagName);
+        return $response['result'] ?? [];
+    }
+}
---
+++ src/Service/RiverFileDataSource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace JournalMedia\Sample\ApiProject\Service;
+
+class RiverFileDataSource implements RiverDatasourceInterface
+{
+    const RESOURCES_EXTENSION = 'json';
+
+    private string $riverSourceBasePath;
+
+    public function __construct(string $riverSourceBasePath)
+    {
+        $this->riverSourceBasePath = $riverSourceBasePath;
+    }
+
+    public function getArticlesByPublication(string $publicationName, array $extraOptions = [])
+    {
+        $resourcePath = $this->riverSourceBasePath . DIRECTORY_SEPARATOR .
+            $publicationName . '.' . self::RESOURCES_EXTENSION;
+        return $this->getAndParseDataFromFile($resourcePath);
+    }
+
+    public function getArticlesByTag(string $tagName, array $extraOptions = [])
+    {
+        $resourcePath = $this->riverSourceBasePath . DIRECTORY_SEPARATOR .
+            $tagName . '.' . self::RESOURCES_EXTENSION;
+        return $this->getAndParseDataFromFile($resourcePath);
+    }
+
+    /**
+     * @param string $resourcePath
+     * @return array|mixed
+     */
+    private function getAndParseDataFromFile(string $resourcePath): mixed
+    {
+        if ($result = file_get_contents($resourcePath)) {
+            return json_decode($result, true);
+        } else {
+            return [];
+        }
+    }
+}
